09904bbb3af8a9f7d0c643878dc46611c28821a1
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-09-07 15:21:05 -0600

    S6

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,10 @@
+﻿//La operación completa incluye crear un origen de datos, definir la expresión de
+//consulta y ejecutar la consulta en una instrucción foreach.
+
+int[] Score = { 97, 92, 81, 60 };
+
+var Query = (from x in Score where x > 80 select x);
+
+Console.WriteLine(string.Join(", ", Query));
+
+Console.WriteLine("Creado por: Jhonson Leiva 愛");
